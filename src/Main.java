394eb831534db0821265dff273659de7252dfebc
MichalRzavucki <michal.rzavucki@gmail.com>
2023-02-25 03:26:52 +0300

    Add Name and Surname to main method

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Michal");
+        System.out.println("Rzavucki");
+        System.out.println("IT Specialist");
+        System.out.println("DJ-ing, Music composing");
+    }
+}
